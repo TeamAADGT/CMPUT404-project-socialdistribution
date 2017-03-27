b64d9f6e56774ab254ff1e2b8487914af6d565bc
tiegan <tiegan@ualberta.ca>
2017-03-27 16:24:38 -0600

    Fixed an error w/ public/logged out images

--- social/app/views/post.py
+++ social/app/views/post.py
@@ -177,14 +177,18 @@ class PostDelete(DeleteView):
 def get_upload_file(request, pk):
     post = get_object_or_404(Post, pk=pk)
 
-    user = request.user
-    author = Author.objects.get(user=request.user.id)
-    node = Node.objects.get(host=author.node.host)
-
-    # get_is_authenticated is always True right now
-    if not post.is_upload() or node.share_images is False or node.get_is_authenticated() is False:
-        # doesn't make sense to do this
-        return HttpResponseForbidden()
+    try:
+        user = request.user
+        author = Author.objects.get(user=request.user.id)
+        node = Node.objects.get(host=author.node.host)
+    except:
+	if post.visibility != "PUBLIC":
+            return HttpResponseForbidden()
+    else:
+	# get_is_authenticated is always True right now
+        if not post.is_upload() or node.share_images is False or node.get_is_authenticated() is False:
+            # doesn't make sense to do this
+            return HttpResponseForbidden()
 
     if post.is_image():
         content = base64.b64decode(post.content)
