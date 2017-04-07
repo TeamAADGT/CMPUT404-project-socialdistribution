c2ae16b0ea427de59a2e6b5fc2652b7b1c99c608
Alanna McLafferty <mclaffer@ualberta.ca>
2017-04-07 17:19:27 -0600

    service/posts/comments works for get

--- service/comments/views.py
+++ service/comments/views.py
@@ -1,7 +1,5 @@
 from rest_framework import generics
 
-from django.shortcuts import get_object_or_404
-
 from social.app.models.comment import Comment
 from social.app.models.post import Post
 from service.authentication.node_basic import NodeBasicAuthentication
@@ -9,32 +7,31 @@ from service.comments.pagination import CommentsPagination
 from service.comments.serializers import CommentSerializer
 
 
-# List view
 class CommentListView(generics.ListAPIView):
     pagination_class = CommentsPagination
     serializer_class = CommentSerializer
     authentication_classes = (NodeBasicAuthentication,)
-    # No permission class
 
     def get_queryset(self):
         remote_node = self.request.user
+        anonymous_node = remote_node is None or not remote_node.is_authenticated
 
-        if remote_node is not None and not remote_node.share_posts:
+        if not anonymous_node and not remote_node.share_posts:
             # We're specifically blocking this node, so short-circuit and return nothing
             return Comment.objects.none()
 
         post_id = self.kwargs["pk"]
         queryset = Comment.objects.filter(post_id=post_id)
-        queryset = queryset.filter(author__node__local=True)
+        queryset = queryset.filter(post__author__node__local=True)
 
-        if remote_node is None:
+        if anonymous_node:
             # Only send 'em public post's comments if we don't know them, or they're asking for just these
             queryset = queryset.filter(post__visibility="PUBLIC")
         else:
             # We trust this node, so send them everything
             queryset = queryset.exclude(post__visibility="SERVERONLY")
 
-        if remote_node is None or not remote_node.share_images:
+        if anonymous_node or not remote_node.share_images:
             # If a node isn't authenticated or we just decided to not do it, don't send over comments on images
             queryset = queryset.filter(post__content_type__in=[key for (key, value) in Post.TEXT_CONTENT_TYPES])
 
--- service/urls.py
+++ service/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import url, include
-from rest_framework import routers
 
 import service.authors.views
 import service.friendrequest.views
@@ -8,8 +7,6 @@ import service.comments.views
 
 import service.internal.authors.views
 
-# router = routers.DefaultRouter()
-# router.register(r'author', service.authors.views.AuthorViewSet, base_name="author")
 
 # Viewset methods are mapped to URLs manually to get around issue where the API schema wouldn't show all available
 # endpoints, causing problems in Swagger
@@ -39,23 +36,21 @@ internal_urls = [
     url(r'^author/(?P<pk>[0-9a-fA-F-]+)/friendrequest/$',
         service.internal.authors.views.friendrequest,
         name='author-friendrequest'),
-    url(r'^author/(?P<pk>[0-9a-z\\-]+)/posts/S',
-        service.posts.views.AuthorPostsList.as_view(),
-        name='author-posts-list'),
 ]
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browseable API.
 service_urls = [
-    url(r'^author/$', include(author_urls)),
-    url(r'^internal/$', include(internal_urls, namespace="internal")),
-    url(r'^auth/$', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^friendrequest/$', service.friendrequest.views.friendrequest, name='friend-request'),
-    url(r'^posts/(?P<pk>[0-9a-z\\-]+)/comments/$', service.comments.views.CommentListView.as_view(), name='comment-detail'),
-    url(r'^posts/(?P<pk>[0-9a-z\\-]+)/$', service.posts.views.AllPostsViewSet.as_view({"get": "retrieve"}),
+    url(r'^author/(?P<pk>[0-9a-z\\-]+)/posts/',service.posts.views.AuthorPostsView.as_view(),
+        name='author-posts-list'),
+    url(r'^author/', include(author_urls)),
+    url(r'^internal/', include(internal_urls, namespace="internal")),
+    url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^friendrequest/', service.friendrequest.views.friendrequest, name='friend-request'),
+    url(r'^posts/(?P<pk>[0-9a-z\\-]+)/comments/', service.comments.views.CommentListView.as_view(), name='comment-detail'),
+    url(r'^posts/(?P<pk>[0-9a-z\\-]+)/', service.posts.views.AllPostsViewSet.as_view({"get": "retrieve"}),
         name='post-detail'),
-    url(r'^posts/$', service.posts.views.PublicPostsList.as_view(), name='public-posts-list'),
-
+    url(r'^posts/', service.posts.views.PublicPostsList.as_view(), name='public-posts-list'),
 ]
 
 urlpatterns = [
