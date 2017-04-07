4a5a63cc892d85f7ebe6578306b10e51ee862da5
Alanna McLafferty <mclaffer@ualberta.ca>
2017-04-07 16:51:39 -0600

    service works for a post's comments now

--- service/comments/serializers.py
+++ service/comments/serializers.py
@@ -1,15 +1,12 @@
 from rest_framework import serializers
 
-#from service.posts.serializers import PostSerializer
 from service.authors.serializers import SimpleAuthorSerializer
-
 from social.app.models.comment import Comment
 
 
 class CommentSerializer(serializers.HyperlinkedModelSerializer):
     author = SimpleAuthorSerializer()
-    #post = PostSerializer()
 
     class Meta:
         model = Comment
-        fields = ("id", "author", "comment", "published")
+        fields = ("id", "author", "text", "published")
--- service/comments/views.py
+++ service/comments/views.py
@@ -1,20 +1,13 @@
+from rest_framework import generics
+
+from django.shortcuts import get_object_or_404
+
 from social.app.models.comment import Comment
 from social.app.models.post import Post
-
 from service.authentication.node_basic import NodeBasicAuthentication
 from service.comments.pagination import CommentsPagination
 from service.comments.serializers import CommentSerializer
 
-from django.shortcuts import render, get_object_or_404
-
-from rest_framework import generics, viewsets
-
-
-def post_comments(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    context = dict()
-    context["all_comments"] = Comment.objects.filter(post_id=post.id)
-    return render(request,'/social/app/template/post/comments.html', context)
 
 # List view
 class CommentListView(generics.ListAPIView):
@@ -26,8 +19,23 @@ class CommentListView(generics.ListAPIView):
     def get_queryset(self):
         remote_node = self.request.user
 
-        post = get_object_or_404(Post, pk=pk)
-        queryset = Comment.objects.filter(post_id=post.id)
-        return queryset
+        if remote_node is not None and not remote_node.share_posts:
+            # We're specifically blocking this node, so short-circuit and return nothing
+            return Comment.objects.none()
 
-# Update view
+        post_id = self.kwargs["pk"]
+        queryset = Comment.objects.filter(post_id=post_id)
+        queryset = queryset.filter(author__node__local=True)
+
+        if remote_node is None:
+            # Only send 'em public post's comments if we don't know them, or they're asking for just these
+            queryset = queryset.filter(post__visibility="PUBLIC")
+        else:
+            # We trust this node, so send them everything
+            queryset = queryset.exclude(post__visibility="SERVERONLY")
+
+        if remote_node is None or not remote_node.share_images:
+            # If a node isn't authenticated or we just decided to not do it, don't send over comments on images
+            queryset = queryset.filter(post__content_type__in=[key for (key, value) in Post.TEXT_CONTENT_TYPES])
+
+        return queryset
