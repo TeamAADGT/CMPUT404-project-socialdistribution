713e83df17827c9b9ecbbbf198f3f92bddaa5116
Adam Ford <adamjford@gmail.com>
2017-04-10 18:29:25 -0600

    Fix indentation

--- social/app/models/post.py
+++ social/app/models/post.py
@@ -245,51 +245,49 @@ def get_remote_node_posts():
     node_posts = list()
     for node in Node.objects.filter(local=False):
         try:
-            all_public_posts_jsons = node.get_all_public_posts(size=50)
-            for all_public_posts_json in all_public_posts_jsons:
-                some_json = all_public_posts_json
-
-                if len(some_json) == 0:
+            json_payloads_list = node.get_all_public_posts()
+            for json_payload in json_payloads_list:
+                if len(json_payload) == 0:
                     continue
 
-            for post_json in some_json['posts']:
-                author_json = post_json['author']
+                for post_json in json_payload['posts']:
+                    author_json = post_json['author']
 
-                # 'id' should be a URI per the spec, but we're being generous and also accepting a straight UUID
-                if author_json['id'].startswith('http'):
-                    remote_author_id = uuid.UUID(Author.get_id_from_uri(author_json['id']))
-                else:
-                    remote_author_id = uuid.UUID(author_json['id'])
+                    # 'id' should be a URI per the spec, but we're being generous and also accepting a straight UUID
+                    if author_json['id'].startswith('http'):
+                        remote_author_id = uuid.UUID(Author.get_id_from_uri(author_json['id']))
+                    else:
+                        remote_author_id = uuid.UUID(author_json['id'])
 
-                # Add remote author to DB
-                author, created = Author.objects.update_or_create(
-                    id=remote_author_id,
-                    defaults={
-                        'node': node,
-                        'displayName': author_json['displayName'],
-                    }
-                )
-                if post_json['id'].startswith('http'):
-                    post_id = uuid.UUID(Post.get_id_from_uri(post_json['id']))
-                else:
-                    post_id = uuid.UUID(post_json['id'])
+                    # Add remote author to DB
+                    author, created = Author.objects.update_or_create(
+                        id=remote_author_id,
+                        defaults={
+                            'node': node,
+                            'displayName': author_json['displayName'],
+                        }
+                    )
+                    if post_json['id'].startswith('http'):
+                        post_id = uuid.UUID(Post.get_id_from_uri(post_json['id']))
+                    else:
+                        post_id = uuid.UUID(post_json['id'])
 
-                # Add remote post to DB
-                post, created = Post.objects.update_or_create(
-                    id=post_id,
-                    defaults={
-                        'title': post_json['title'],
-                        'source': post_json['source'],
-                        'origin': post_json['origin'],
-                        'description': post_json['description'],
-                        'author': author,
-                        'published': post_json['published'],
-                        'content': post_json['content'],
-                        'content_type': post_json['contentType'],
-                        'visibility': post_json['visibility'],
-                    }
-                )
-                node_posts.append(post)
+                    # Add remote post to DB
+                    post, created = Post.objects.update_or_create(
+                        id=post_id,
+                        defaults={
+                            'title': post_json['title'],
+                            'source': post_json['source'],
+                            'origin': post_json['origin'],
+                            'description': post_json['description'],
+                            'author': author,
+                            'published': post_json['published'],
+                            'content': post_json['content'],
+                            'content_type': post_json['contentType'],
+                            'visibility': post_json['visibility'],
+                        }
+                    )
+                    node_posts.append(post)
 
         except Exception, e:
             logging.error(e)
@@ -322,7 +320,8 @@ def get_all_remote_node_posts():
                         'displayName': author_json['displayName'],
                     }
                 )
-                if 'http' in post_json['id']:
+
+                if post_json['id'].startswith('http'):
                     post_id = uuid.UUID(Post.get_id_from_uri(post_json['id']))
                 else:
                     post_id = uuid.UUID(post_json['id'])
