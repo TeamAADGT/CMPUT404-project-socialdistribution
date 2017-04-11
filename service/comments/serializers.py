40a7d0a7a53f9563957cf21a7d1eaf7163d23e1c
Adam Ford <adamjford@gmail.com>
2017-04-10 22:30:50 -0600

    Try to fix error

--- service/comments/serializers.py
+++ service/comments/serializers.py
@@ -34,22 +34,20 @@ class CreateCommentSerializer(serializers.Serializer):
     comment = NewCommentSerializer()
 
     # TODO: make sure you check that the post is a local post!
-    def validate_post(self, value):
+    def validate(self, data):
         """
         We do not allow anonymous comments -- all comments must be submitted by known remote nodes
         Must ensure that remote author has permission to comment on that post.
         """
         # value looks like: http://127.0.0.1:8000/posts/563aeed8-37f5-4aa9-bf7c-6e012429c4a3/
-        full_url_post_id = value
-        post_id = Post.get_id_from_uri(full_url_post_id)
+        post_id = Post.get_id_from_uri(data['post'])
+
         post = get_object_or_404(Post, pk=post_id)
-        post_author = get_object_or_404(Author, pk=post.author_id)
 
         # Get this from the request
-        full_url_remote_author_id = self.initial_data["comment"]["author"]["url"]
-        remote_author_id = Author.get_id_from_uri(full_url_remote_author_id)
 
-        remote_node_host = self.initial_data["comment"]["author"]["host"]
+        print data
+        remote_node_host = data["comment"]["author"]["host"]
         remote_node = get_object_or_404(Node, service_url=remote_node_host)
         anonymous_node = remote_node is None or not remote_node.is_authenticated
 
@@ -63,38 +61,44 @@ class CreateCommentSerializer(serializers.Serializer):
 
         else:  # We trust this node, so allow them to comment on this post IF the remote author has permission to view
             # that post.
-            queryset = Post.objects.filter(id=post_id)
-            queryset = queryset.filter(author__node__local=True)  # post must be local
+            remote_author_uri = data["comment"]["author"]["url"]
+            remote_author_id = Author.get_id_from_uri(remote_author_uri)
+            remote_author = remote_node.create_or_update_remote_author(remote_author_id)
+
+            queryset = Post.objects.filter(id=post_id, author__node__local=True)
             queryset = queryset.exclude(visibility="SERVERONLY")  # post must not be local only
 
             if post.visibility == "PUBLIC" and queryset:
-                return value
+                return data
 
             # Check if remote author is friends with post author
-            is_friend = post_author.friends_with_remote_author(remote_author_id)
-            if post.visibility == "FRIENDS" and is_friend and queryset:
-                return value
+            if post.visibility == "FRIENDS":
+                is_friend = post.author.friends_with(remote_author)
+                if is_friend and queryset:
+                    return data
 
-            # Check if remote author is in the visibleTo list for that post
-            is_visible_to = post.visible_to_remote_author(remote_author_id)
-            if post.visibility == "PRIVATE" and is_visible_to and queryset:
-                return value
+                # Check if remote author is in the visibleTo list for that post
+            if post.visibility == "PRIVATE":
+                is_visible_to = post.visible_to_remote_author(remote_author_id)
+                if is_visible_to and queryset:
+                    return data
 
-            # TODO: Check if remote author is FOAF to post author
-            is_FOAF = False
-            if post.visibility == "FOAF" and is_FOAF and queryset:
-                return value
+            if post.visibility == "FOAF":
+                # TODO: Check if remote author is FOAF to post author
+                is_FOAF = False
+                if is_FOAF and queryset:
+                    return data
 
             # remote author does not have permission to post a comment
             else:
                 raise ValidationError('You do not have permission to comment on that post')
 
     def create(self, validated_data):
-        comment_data = self.initial_data["comment"]
+        comment_data = validated_data["comment"]
         comment_id = comment_data["id"]
 
         # We have already checked that the post is local so we know it must already be in the DB
-        full_url_post_id = self.initial_data["post"]
+        full_url_post_id = validated_data["post"]
         post_id = Post.get_id_from_uri(full_url_post_id)
         post = get_object_or_404(Post, pk=post_id)
 
